9c375a4412a2899f4e85139d0a00ee22b46a0ca6
matrixster <muteti@gmail.com>
2014-12-18 09:36:53 +0300

    changes to reflect the new no-php end
    Yes

--- p/config.php
+++ p/config.php
@@ -1,3 +1,11 @@
 <?php
-eval(gzinflate(base64_decode('XY7NTsMwEITPWPI7rKoeSlWSO6gSEhXiUE7kwA257qaxcLzG69T07dnwG+qL7fl2ZlbVS63uKJ6SO3QZNg/N4/YZts2mgi7neF3XpZRq3+Xev1eWeq2azjEcMbGjANTCE7W5mIQgcpsQoaUEA7twABcgULgSW4/JOuPBxOidNVm8XGl1L6MTOjBC9GjkshSysRnYeOTbv37IBLTLRqIlCAOjVstaK61Azrw/8Zt/6YgzrGHmyRo/fmY3/7j0pJEnonMUDZcRncn7nbxFLuY4jOgbehdeRf6akZUD2ryYLLGaNq6mHZcScjGX2B83oxfzWLT47fwc0uoD')));
+//eval(gzinflate(base64_decode('XY7NTsMwEITPWPI7rKoeSlWSO6gSEhXiUE7kwA257qaxcLzG69T07dnwG+qL7fl2ZlbVS63uKJ6SO3QZNg/N4/YZts2mgi7neF3XpZRq3+Xev1eWeq2azjEcMbGjANTCE7W5mIQgcpsQoaUEA7twABcgULgSW4/JOuPBxOidNVm8XGl1L6MTOjBC9GjkshSysRnYeOTbv37IBLTLRqIlCAOjVstaK61Azrw/8Zt/6YgzrGHmyRo/fmY3/7j0pJEnonMUDZcRncn7nbxFLuY4jOgbehdeRf6akZUD2ryYLLGaNq6mHZcScjGX2B83oxfzWLT47fwc0uoD')));
+$mysql_host = "41.242.2.139";
+$mysql_user = "wavuh_admin";
+$mysql_pasw = "@Wavuh456";
+$mysql_db   = "wavuh_456";
+
+$link = mysql_connect($mysql_host, $mysql_user, $mysql_pasw);
+$db = mysql_select_db ($mysql_db);
+
 ?>
